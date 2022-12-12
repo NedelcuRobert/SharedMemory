efd897beea2eebb8bbfc455d34d4b625a461a918
NedelcuRobert <nedelcurobert62@gmail.com>
2022-12-12 23:33:29 +0200

    added writer and reader files

--- writer.c
+++ writer.c
@@ -1,8 +1,8 @@
-
-#include <sys/types.h>
-#include <sys/mman.h>
+#include <unistd.h>
 #include <semaphore.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/fcntl.h>
 #include <unistd.h>
@@ -12,20 +12,30 @@
 char shm_fn[] = "my_shm";
 char sem_fn[] = "my_sem";
 
-/**** READER ****/
+/**** WRITER ****/
 
 int main() {
     caddr_t shmptr;
+    unsigned int mode;
     int shmdes, index;
     sem_t* semdes;
     int SHM_SIZE;
 
+    mode = S_IRWXU | S_IRWXG;
+
     /* Open the shared memory object */
+
+    if ((shmdes = shm_open(shm_fn, O_CREAT | O_RDWR | O_TRUNC, mode)) == -1) {
+        perror("shm_open failure");
+        exit(0);
+    }
+
+    /* Preallocate a shared memory area */
 
     SHM_SIZE = sysconf(_SC_PAGE_SIZE);
 
-    if ((shmdes = shm_open(shm_fn, O_RDWR, 0)) == -1) {
-        perror("shm_open failure");
+    if (ftruncate(shmdes, SHM_SIZE) == -1) {
+        perror("ftruncate failure");
         exit(0);
     }
 
@@ -35,29 +45,25 @@ int main() {
         exit(0);
     }
 
-    /* Open the Semaphore */
+    /* Create a semaphore in locked state */
 
-    semdes = sem_open(sem_fn, 0, 0644, 0);
+    semdes = sem_open(sem_fn, O_CREAT, 0644, 0);
 
     if (semdes == (void*)-1) {
         perror("sem_open failure");
         exit(0);
     }
 
-    /* Lock the semaphore */
+    /* Access to the shared memory area */
 
-    if (!sem_wait(semdes)) {
-
-        /* Access to the shared memory area */
-
-        for (index = 1; index <= 1000; index++)
-            printf("The shared memory shmptr[%d] = %d\n", index, shmptr[index]);
-
-        /* Release the semaphore lock */
-
-        sem_post(semdes);
+    for (index = 1; index <= 1000; index++) {
+        printf("write %d into the shared memory shmptr[%d]\n", index, index);
+        shmptr[index] = index;
     }
 
+    /* Release the semaphore lock */
+
+    sem_post(semdes);
     munmap(shmptr, SHM_SIZE);
 
     /* Close the shared memory object */
@@ -67,5 +73,8 @@ int main() {
     /* Close the Semaphore */
 
     sem_close(semdes);
-    sem_unlink(sem_fn);
+
+    /* Delete the shared memory object */
+
+    shm_unlink(shm_fn);
 }
